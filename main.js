03240517e9c4d8c35279509b0744ab94d332a71c
Yamilla Prodhan <92693396+yamillap@users.noreply.github.com>
2023-01-16 11:33:55 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+import './page.css'
+
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector('#geo'),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize( window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+
+renderer.render(scene, camera);
+
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100)
+const material = new THREE.MeshBasicMaterial({color: 0x9681b0, wireframe: true });
+const torus = new THREE.Mesh(geometry, material);
+
+scene.add(torus)
+
+function animate(){
+    requestAnimationFrame(animate);
+
+    torus.rotation.x += 0.01;
+    torus.rotation.y += 0.005;
+    torus.rotation.z += 0.01;
+    
+    renderer.render(scene, camera);
+}
+
+animate();
+
+
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.nav-links');
+
+menu.onclick = () => {
+    menu.classList.toggle('uil-bars');
+    navbar.classList.toggle('open');
+}
