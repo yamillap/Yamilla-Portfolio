3777a19e5f221d292a19c8f39a2c62b6b52b8c02
Yamilla Prodhan <92693396+yamillap@users.noreply.github.com>
2023-10-17 22:55:31 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
+import './contact.html'
+
+
+
+
+// let menu = document.querySelector('#menu-icon');
+// let navbar = document.querySelector('.nav-links');
+
+// menu.onclick = () => {
+//     // menu.classList.toggle('menu-icon');
+//     // navbar.classList.toggle('open');
+
+// }
+
+
+
+
+
+
 
 
 
