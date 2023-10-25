510a4fb47f7eb3044c3ddd9dd78d9f5af4fc5579
Yamilla Prodhan <92693396+yamillap@users.noreply.github.com>
2023-10-25 15:37:41 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-
+const menu;
+const navbar;
 window.onscroll = function() {nav_menu()};
 function nav_menu(){
-    const menu = document.getElementById("menu-icon");
-    const navbar = document.getElementsByClassName('nav-links')[0];
-
+    menu =  document.getElementById("menu-icon");
+    navbar = document.getElementsByClassName('nav-links')[0];
     menu.classList.toggle('.menu-icon');
     navbar.classList.toggle('open');
 
